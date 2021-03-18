e7afec5fbe15356aa6987b786729dea2ca3cff79
Dhakernator24 <davidn.snow@hotmail.es>
2021-03-18 09:47:08 +0100

    Prueba con entrada std

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,5 +1,20 @@
 package src;
 
-public class Prueba {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
+public class Prueba{
+	public static void main(String[] args) {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		int numberCases = 0;
+		try {
+			//number of cases
+			line = input.readLine();
+			numberCases = Integer.parseInt(line);
+		}catch(IOException e) {
+			
+		}
+	}
 }
